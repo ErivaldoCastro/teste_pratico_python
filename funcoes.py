c9af4e995f5eb0afc2571629d8266f3573343eb5
ErivaldoCastro <95341495+ErivaldoCastro@users.noreply.github.com>
2022-11-14 23:13:04 -0300

    finish

--- funcoes.py
+++ funcoes.py
@@ -1,26 +1,5 @@
 import pandas as pd
-from bd import Empresa
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-def RetornaSession():
-    USUARIO = "root"
-    SENHA = ""
-    HOST = "localhost"
-    BANCO = "testepython"
-    PORT = "3306"
-
-    CONN = f"mysql+pymysql://{USUARIO}:{SENHA}@{HOST}:{PORT}/{BANCO}"
-
-    engine = create_engine(CONN, echo=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    Base = declarative_base()
-    return Session()
-
-
-session = RetornaSession()
 
 
 def transformar_padrao_data(dataframe, nome_coluna):
@@ -38,22 +17,3 @@ def tirar_mascara(dataframe, nome_coluna):
 
 
 
-#lista de nome na planilha
-titulos = ['Originador','_1','Cedente','_3','CCB','Id','Cliente','_7','Endereço','CEP','Cidade','UF','_12','_13',
-                '_14','_15','_16','_17','_18','_19','_20','Multa','Mora','_23','_24','_25','_26']
-
-
-#lista de nomes no banco de dados
-titulos_bd= ['Originador','Doc_Originador','Cedente','Doc_Cedente','Ccb','Id_cliente','Cliente','Cpf_cnpj','Endereço','Cep',
-    'Cidade','Uf','Valor_do_emprestimo','Taxa_de_juros','Parcela_em_reais','Principal','Juros','Iof','Comissao','Total_parcelas',
-    'Parcelas','Multa','Mora','Data_de_emissao','Data_de_vencimento','Data_de_compra','preco_de_aquisicao']
-
-
-
-def Insert_bd(datafram,nome_coluna_planilha,nome_coluna_bd):
-
-    for i in datafram[nome_coluna_planilha]:
-        x = Empresa(nome_coluna_bd=i)
-    
-    session.add(x)
-    session.commit()
--- teste_pratico.py
+++ teste_pratico.py
@@ -40,7 +40,7 @@ transformar_padrao_data(tabela,'Data de Compra CCB')
 #Tirar as mascaras CPF/CNPJ
 tirar_mascara(tabela,'CPF/CNPJ')
 
-'''
+#Savar no banco de dados
 for i in tabela.itertuples(index=False):
 
     x = Empresa(Originador=i.Originador,Doc_Originador=i._1,Cedente=i.Cedente,Doc_Cedente=i._3,
@@ -50,20 +50,6 @@ for i in tabela.itertuples(index=False):
     Mora=i.Mora,Data_de_emissao=i._23,Data_de_vencimento=i._24,Data_de_compra=i._25,preco_de_aquisicao=i._26)
     session.add(x)
     session.commit()
-'''
-
-#lista de nome na planilha
-titulos = ['Originador','Doc Originador','Cedente','Doc Cedente','CCB','Id','Cliente','CPF/CNPJ','Endereço','CEP','Cidade','UF',
-            'Valor do Empréstimo','Taxa de Juros (a.m.)','Parcela R$','Principal R$','Juros R$','IOF R$','Comissão R$',
-            'Total Parcelas','Parcela #','Multa','Mora','Data de Emissão','Data de Vencimento','Data de Compra CCB','Preço de Aquisição']
-
-
-
-#lista de nomes no banco de dados
-titulos_bd= ['Originador','Doc_Originador','Cedente','Doc_Cedente','Ccb','Id_cliente','Cliente','Cpf_cnpj','Endereço','Cep',
-    'Cidade','Uf','Valor_do_emprestimo','Taxa_de_juros','Parcela_em_reais','Principal','Juros','Iof','Comissao','Total_parcelas',
-    'Parcelas','Multa','Mora','Data_de_emissao','Data_de_vencimento','Data_de_compra','preco_de_aquisicao']
-
 
 
 
